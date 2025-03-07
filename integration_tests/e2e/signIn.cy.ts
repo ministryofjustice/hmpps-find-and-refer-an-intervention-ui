361eeee8f063680c16e20d231435385e08255ac5
nia evans <nia.evans@digital.justice.gov.uk>
2025-03-07 14:44:59 +0000

    Re add integration tests

---
+++ integration_tests/e2e/signIn.cy.ts
@@ -0,0 +1,9 @@
+import IndexPage from '../pages/index'
+import AuthSignInPage from '../pages/authSignIn'
+import Page from '../pages/page'
+import AuthManageDetailsPage from '../pages/authManageDetails'
+import Unauthorised from '../pages/unauthorised'
+
+context('Sign In', () => {
+  it('Unauthenticated user directed to auth', () => {})
+})
