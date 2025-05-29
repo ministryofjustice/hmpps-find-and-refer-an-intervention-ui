38dd48dddbeaf11efc5953b2ba1ba134df084575
nia evans <nia.evans@digital.justice.gov.uk>
2025-05-29 12:37:24 +0100

    QA review comments

--- testutils/factories/serviceUserDetails.ts
+++ testutils/factories/serviceUserDetails.ts
@@ -4,7 +4,7 @@ import ServiceUserDetails from '../../server/models/serviceUserDetails'
 export default Factory.define<ServiceUserDetails>(() => ({
   name: 'Bob',
   crn: 'X123456',
-  dob: '20-05-1990',
+  dob: '1990-05-20',
   gender: 'Male',
   ethnicity: 'British',
   currentPdu: 'East Sussex',
